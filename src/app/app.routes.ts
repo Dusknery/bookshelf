685a6a0b6915779edeab43c46a991c5158658448
Dusknery <linnea.ryd05@gmail.com>
2026-04-21 12:29:41 +0200

    pages setup 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Booklist } from "./pages/booklist/booklist";   
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'booklist', component: Booklist }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Header } from './components/header/header';
+import { Booklist } from "./pages/booklist/booklist";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home, Booklist],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
